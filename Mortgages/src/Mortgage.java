2f427fe8669a0245b2ca95180f44c5a84a23c945
Breanna Mackey <mackey6@clemson.edu>
2023-10-24 13:56:38 -0400

    all files

--- Mortgages/src/Mortgage.java
+++ Mortgages/src/Mortgage.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Mortgage {
+import cpsc2150.banking.models.AbsMortgage;
+import cpsc2150.banking.models.IMortgage;
+
+public class Mortgage extends AbsMortgage {
+    //constructor
+    //checks if loan approved
+    /* loanApproved iff (Rate*12 < RATE_TOO_HIGH AND PercentDown >= MIN_PERCENT_DOWN AND DebtToIncomeRatio <= DTOITOOHIGH) AND
+        *          Payment = #Payment AND Rate = #Rate AND Customer = #Customer AND DebtToIncomeRatio = #DebtToIncomeRatio AND 
+        *          Principal = #Principal AND NumberOfPayments = #NumberOfPayments AND PercentDown = #PercentDown */
+    boolean loanApproved(){
+        double rate = getRate();
+        if(rate*12 < RATETOOHIGH && percentDown >= MIN_PERCENT_DOWN && DebtToIncomeRatio <= DTOITOOHIGH){
+            //loan approved
+        }
+
+    }
+    double getPayment(){
+
+    }
+    double getRate(){
+
+    }
+    double getPrincipal(){
+
+    }
+    int getYears(){
+
+    }
 }
