7be6a1b9b5771f8d79480ebc9993176725f2ca26
Breanna Mackey <mackey6@clemson.edu>
2023-10-31 13:07:24 -0400

    last tues

--- Mortgages/src/Mortgage.java
+++ Mortgages/src/Mortgage.java
@@ -1,29 +1,41 @@
 import cpsc2150.banking.models.AbsMortgage;
 import cpsc2150.banking.models.IMortgage;
 
-public class Mortgage extends AbsMortgage {
+//INVARIANT AND CORRESPONDENT
+
+public class Mortgage extends AbsMortgage implement IMortgage{
+    private double rate;
+    private double payment;
+    private double principal;
+    private int years;
     //constructor
+    public Mortgage(double r, double pay, double p, int y){
+        rate = r;
+        payment = pay;
+        principal = p;
+        years = y;
+    }
     //checks if loan approved
     /* loanApproved iff (Rate*12 < RATE_TOO_HIGH AND PercentDown >= MIN_PERCENT_DOWN AND DebtToIncomeRatio <= DTOITOOHIGH) AND
         *          Payment = #Payment AND Rate = #Rate AND Customer = #Customer AND DebtToIncomeRatio = #DebtToIncomeRatio AND 
         *          Principal = #Principal AND NumberOfPayments = #NumberOfPayments AND PercentDown = #PercentDown */
-    boolean loanApproved(){
+    public boolean loanApproved(){
         double rate = getRate();
         if(rate*12 < RATETOOHIGH && percentDown >= MIN_PERCENT_DOWN && DebtToIncomeRatio <= DTOITOOHIGH){
             //loan approved
         }
 
     }
-    double getPayment(){
+    public double getPayment(){
 
     }
-    double getRate(){
+    public double getRate(){
 
     }
-    double getPrincipal(){
+    public double getPrincipal(){
 
     }
-    int getYears(){
+    public int getYears(){
 
     }
 }
