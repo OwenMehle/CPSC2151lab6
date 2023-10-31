3b6dd2821680f17569a5a7e56b6a6690d2231ee1
coal719 <cashb719@gmail.com>
2023-10-31 13:30:35 -0400

    more stuff

--- Mortgages/src/Mortgage.java
+++ Mortgages/src/Mortgage.java
@@ -5,14 +5,14 @@ import cpsc2150.banking.models.ICustomer;
 public class Mortgage extends AbsMortgage {
     private double houseCost;
     private double downPayment;
-    private double years;
-    private ICustomer Customer;
+    private int years;
+    private ICustomer customer;
     //constructor
     public Mortgage(double cost, double dpayment, int timeYears, ICustomer Customer) {
         houseCost = cost;
         downPayment = dpayment;
         years = timeYears;
-        Customer = new Customer();
+        customer = Customer;
     }
     //checks if loan approved
     /* loanApproved iff (Rate*12 < RATE_TOO_HIGH AND PercentDown >= MIN_PERCENT_DOWN AND DebtToIncomeRatio <= DTOITOOHIGH) AND
@@ -30,12 +30,33 @@ public class Mortgage extends AbsMortgage {
         return
     }
     double getRate(){
+        double APR = .025;
+        if (years > 30) {
+            APR += .005;
+        } else {
+            APR += .01;
+        }
+        if (downPayment / houseCost > .2) {
+            APR += .005;
+        }
+        if (customer.getCreditScore() < 500) {
+            APR += .1;
+        } else if (customer.getCreditScore() >= 500 && customer.getCreditScore() < 600) {
+            APR += .05;
+        } else if (customer.getCreditScore() >= 600 && customer.getCreditScore() < 700) {
+            APR += .01;
+        } else if (customer.getCreditScore() >= 700 && customer.getCreditScore() < 800) {
+            APR += .005;
+        } else if (customer.getCreditScore() >= 750 && customer.getCreditScore() <= 850) {
+            APR += 0;
+        }
 
+        return APR;
     }
     double getPrincipal(){
-
+        return houseCost - downPayment;
     }
     int getYears(){
-
+        return years;
     }
 }
