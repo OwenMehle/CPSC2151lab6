3f1b5e1e5de534c1e708388e3fcb93b97f34594d
coal719 <cashb719@gmail.com>
2023-10-31 13:41:45 -0400

    Revert "Merge branch 'main' of https://github.com/OwenMehle/CPSC2151lab6"
    This reverts commit df4f8f625a3896929ee286df804f76329427c206, reversing
    changes made to 3b6dd2821680f17569a5a7e56b6a6690d2231ee1.

--- Mortgages/src/Mortgage.java
+++ Mortgages/src/Mortgage.java
@@ -2,21 +2,6 @@ import cpsc2150.banking.models.AbsMortgage;
 import cpsc2150.banking.models.IMortgage;
 import cpsc2150.banking.models.ICustomer;
 
-<<<<<<< HEAD
-//INVARIANT AND CORRESPONDENT
-
-public class Mortgage extends AbsMortgage implement IMortgage{
-    private double rate;
-    private double payment;
-    private double principal;
-    private int years;
-    //constructor
-    public Mortgage(double r, double pay, double p, int y){
-        rate = r;
-        payment = pay;
-        principal = p;
-        years = y;
-=======
 public class Mortgage extends AbsMortgage {
     private double houseCost;
     private double downPayment;
@@ -33,7 +18,7 @@ public class Mortgage extends AbsMortgage {
     /* loanApproved iff (Rate*12 < RATE_TOO_HIGH AND PercentDown >= MIN_PERCENT_DOWN AND DebtToIncomeRatio <= DTOITOOHIGH) AND
         *          Payment = #Payment AND Rate = #Rate AND Customer = #Customer AND DebtToIncomeRatio = #DebtToIncomeRatio AND 
         *          Principal = #Principal AND NumberOfPayments = #NumberOfPayments AND PercentDown = #PercentDown */
-    public boolean loanApproved(){
+    boolean loanApproved(){
         double rate = getRate();
         if(rate*12 < RATETOOHIGH && percentDown >= MIN_PERCENT_DOWN && DebtToIncomeRatio <= DTOITOOHIGH){
             //loan approved
@@ -41,21 +26,37 @@ public class Mortgage extends AbsMortgage {
         }
 
     }
-<<<<<<< HEAD
-    public double getPayment(){
-
-=======
     double getPayment(){
         return
->>>>>>> 6a46c8504ab5075bc7676f4673d345e4755577f7
     }
     double getRate(){
+        double APR = .025;
+        if (years > 30) {
+            APR += .005;
+        } else {
+            APR += .01;
+        }
+        if (downPayment / houseCost > .2) {
+            APR += .005;
+        }
+        if (customer.getCreditScore() < 500) {
+            APR += .1;
+        } else if (customer.getCreditScore() >= 500 && customer.getCreditScore() < 600) {
+            APR += .05;
+        } else if (customer.getCreditScore() >= 600 && customer.getCreditScore() < 700) {
+            APR += .01;
+        } else if (customer.getCreditScore() >= 700 && customer.getCreditScore() < 800) {
+            APR += .005;
+        } else if (customer.getCreditScore() >= 750 && customer.getCreditScore() <= 850) {
+            APR += 0;
+        }
 
+        return APR;
     }
     double getPrincipal(){
-
+        return houseCost - downPayment;
     }
     int getYears(){
-
+        return years;
     }
 }
