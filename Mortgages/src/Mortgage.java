6a46c8504ab5075bc7676f4673d345e4755577f7
coal719 <cashb719@gmail.com>
2023-10-31 13:06:57 -0400

    stuff

--- Mortgages/src/Mortgage.java
+++ Mortgages/src/Mortgage.java
@@ -1,8 +1,19 @@
 import cpsc2150.banking.models.AbsMortgage;
 import cpsc2150.banking.models.IMortgage;
+import cpsc2150.banking.models.ICustomer;
 
 public class Mortgage extends AbsMortgage {
+    private double houseCost;
+    private double downPayment;
+    private double years;
+    private ICustomer Customer;
     //constructor
+    public Mortgage(double cost, double dpayment, int timeYears, ICustomer Customer) {
+        houseCost = cost;
+        downPayment = dpayment;
+        years = timeYears;
+        Customer = new Customer();
+    }
     //checks if loan approved
     /* loanApproved iff (Rate*12 < RATE_TOO_HIGH AND PercentDown >= MIN_PERCENT_DOWN AND DebtToIncomeRatio <= DTOITOOHIGH) AND
         *          Payment = #Payment AND Rate = #Rate AND Customer = #Customer AND DebtToIncomeRatio = #DebtToIncomeRatio AND 
@@ -11,11 +22,12 @@ public class Mortgage extends AbsMortgage {
         double rate = getRate();
         if(rate*12 < RATETOOHIGH && percentDown >= MIN_PERCENT_DOWN && DebtToIncomeRatio <= DTOITOOHIGH){
             //loan approved
+            return true;
         }
 
     }
     double getPayment(){
-
+        return
     }
     double getRate(){
 
